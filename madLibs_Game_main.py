00059af44a0d8f40941f113ea755dc2692fc1697
MargaritaCa <busy0001@algonquinlive.com>
2021-01-17 23:24:34 -0500

    Mad Libs Game finished

---
+++ madLibs_Game_main.py
@@ -0,0 +1,38 @@
+#Mad Libs game "The Letter from George"
+#Created by Margarita Busygina
+#Python Challenge-day3
+#jan 17 2021
+
+print("Welcome to \"The Letter from George\" Mad Libs Game!")
+plural_noun_1 = input("Enter plural noun: ")
+occupation_2 = input("Enter an occupation(job): ")
+place_3 = input("Enter a place: ")
+number_4 = input("Enter a number: ")
+adjective_5 = input("Enter an adjective: ")
+ing_verb_6 = input("Enter a verb ending with -ing: ")
+plural_noun_7 = input("Enter plural noun: ")
+place_8 = input("Enter a place: ")
+adjective_9 = input("Enter an adjective: ")
+plural_noun_10 = input("Enter plural noun: ")
+ing_verb_11 = input("Enter a verb ending with -ing: ")
+plural_noun_12 = input("Enter plural noun: ")
+adjective_13 = input("Enter an adjective: ")
+noun_14 = input("Enter a noun: ")
+body_part_15 = input("Enter a body part: ")
+verb_16 = input("Enter a verb: ")
+adjective_17 = input("Enter an adjective: ")
+body_part_18 = input("Enter a body part: ")
+
+print("Hello, my fellow " + plural_noun_1
+      + " in 2021, it's me, George Washington, the first " + occupation_2
+      +". \nI am writing from the " +place_3+ ", where I have been secretly living for the past " + number_4
+      +" years. \nI am concerned by the "+adjective_5+" state of affairs in America these days. "
+      +"\nIt seems that your politicians are more concerned with "+ing_verb_6
+      +" one another than with listening to the "+ plural_noun_7
+      +" of the people. \nWhen we declared our independence from the "+place_8
+      +",\nwe set forth on a "+adjective_9+" path guided by the voices of the everyday "+plural_noun_10
+      +".\nIf we're going to keep "+ing_verb_11+", then we need to learn how to respect all "+plural_noun_12
+      +". \nDon't get me wrong; we had "+adjective_13+" problems in my day, too. Benjamin Franklin once called me a "
+      +noun_14+" and kicked me in the "+body_part_15+". \nBut at the end of the day, we were able to "+verb_16
+      +" in harmony. Let us find that "+adjective_17+" spirit once again, \nor else I'm taking my "+body_part_18
+      +" off the quarter!")
